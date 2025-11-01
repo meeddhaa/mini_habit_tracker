dd6b2c314364152b2d4af0de106d1d7359b079ea
meeddhaa <naafisa.medha@gmail.com>
2025-11-02 00:20:26 +0600

    `Refactor MyHabitTile to use StatefulWidget instead of StatelessWidget and add toggle completion functionality`

--- lib/components/my_habit_tile.dart
+++ lib/components/my_habit_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyHabitTile extends StatelessWidget {
+class MyHabitTile extends StatefulWidget {
   final String text;
   final bool isHabitCompletedToday;
   final void Function(bool?)? onChanged;
@@ -13,32 +13,47 @@ class MyHabitTile extends StatelessWidget {
   });
 
   @override
+  State<MyHabitTile> createState() => _MyHabitTileState();
+}
+
+class _MyHabitTileState extends State<MyHabitTile> {
+  late bool isCompleted;
+
+  @override
+  void initState() {
+    super.initState();
+    isCompleted = widget.isHabitCompletedToday;
+  }
+
+  void toggleCompletion() {
+    setState(() {
+      isCompleted = !isCompleted;
+    });
+    if (widget.onChanged != null) {
+      widget.onChanged!(isCompleted);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        if (onChanged != null) {
-          // toggle the habit completion status
-          onChanged!(!isHabitCompletedToday);
-        }
-      },
+      onTap: toggleCompletion,
       child: Container(
         decoration: BoxDecoration(
           color:
-              isHabitCompletedToday
-                  ? Colors.deepPurpleAccent
+              isCompleted
+                  ? const Color.fromARGB(255, 149, 117, 237)
                   : Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(
-            12,
-          ), // optional, makes it look nicer
+          borderRadius: BorderRadius.circular(12),
         ),
         padding: const EdgeInsets.all(16.0),
         margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
         child: ListTile(
-          title: Text(text),
+          title: Text(widget.text),
           leading: Checkbox(
-            activeColor: Colors.indigoAccent,
-            value: isHabitCompletedToday,
-            onChanged: onChanged,
+            activeColor: const Color.fromARGB(255, 168, 180, 246),
+            value: isCompleted,
+            onChanged: (_) => toggleCompletion(),
           ),
         ),
       ),
